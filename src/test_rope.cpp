85d5598955a14d3041d1ee676cfb92f983ee8c27
John Schulman <joschu@surgical1.(none)>
2011-11-13 19:51:45 -0800

    modified test_rope.cpp to do playback. removed playback_joints from targets

--- src/test_rope.cpp
+++ src/test_rope.cpp
@@ -1,17 +1,24 @@
 #include "rope.h"
 #include "simplescene.h"
 #include "unistd.h"
+#include "file_reading.h"
+#include "grabbing.h"
+
 using boost::shared_ptr;
+
 int main() {
 
-  int nLinks = 20;
+  const float table_height = .765;
+  const float rope_radius = .01;
+
+  int nLinks = 30;
   btAlignedObjectArray<btVector3> ctrlPts;
   for (int i=0; i< nLinks; i++) {
-    ctrlPts.push_back(btVector3(.05*(i-nLinks/2.)+1,0,2));
+    ctrlPts.push_back(btVector3(.5+.05*i,0,table_height+rope_radius));
   }
 
 
-  shared_ptr <btMotionState> ms(new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(1,0,.765))));
+  shared_ptr <btMotionState> ms(new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(1,0,table_height))));
   shared_ptr<BulletObject> table(new BoxObject(0,btVector3(.75,.75,.01),ms));
 
   shared_ptr<CapsuleRope> ropePtr(new CapsuleRope(ctrlPts,.01));
@@ -20,15 +27,41 @@ int main() {
   s.env->add(ropePtr);
   s.env->add(table);
 
+  vector< vector<double> > joints;
+  vector< int > inds;
+  read_1d_array(inds, "/home/joschu/Data/knot_kinect1/inds.txt");
+  read_2d_array(joints,"/home/joschu/Data/knot_kinect1/vals.txt");
 
   int step = 0;
-  while (!s.viewer.done()) {
+
+  //  btVector3 v = ropePtr->bodies[0]->getCenterOfMassPosition();
+  RobotBase::ManipulatorPtr rarm(s.pr2->robot->GetManipulators()[5]);
+
+  Grab g;
+  for (int i=0; i < joints.size() && !s.viewer.done(); i++) {
+    cout << i << endl;
+    vector<double> joint = joints[i];
+    s.pr2->setDOFValues(inds,joint);
+
+    
+
+    if (i == 160) {
+      btVector3 rhpos = util::toBtTransform(rarm->GetEndEffectorTransform()).getOrigin();
+      g = Grab(ropePtr->bodies[0].get(),rhpos,s.env->bullet->dynamicsWorld);
+    }
+    if (i > 160) {
+      g.updatePosition(util::toBtTransform(rarm->GetEndEffectorTransform()).getOrigin());
+    }
+
+
+    /*
     btRigidBody* body = ropePtr->bodies[0].get();
     cout << "position at time " << ++step << ": " << 
       body->getCenterOfMassPosition().x() << " " <<
       body->getCenterOfMassPosition().y() << " " <<
       body->getCenterOfMassPosition().z() << " " << endl;
-        
+    */
+
     s.step(1/30.,300,.001);
     // usleep(10*1000);
   }
