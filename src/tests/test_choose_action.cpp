c735c615a4f77c035d3a5cd0ba44c3446cd02f8d
Ben Sklaroff <bsklaroff@gmail.com>
2012-02-20 22:21:06 -0800

    trying raytesting for multiple layers of cloth

--- src/tests/test_choose_action.cpp
+++ src/tests/test_choose_action.cpp
@@ -5,13 +5,16 @@
 
 struct ChooseActionScene : public BaseScene {
   ChooseActionScene();
-  btSoftBody* psb;
+  void step(float dt, int maxsteps, float internaldt);
+  int step_count;
+  BulletSoftObject::Ptr cloth;
 };
 
 ChooseActionScene::ChooseActionScene() {
+  step_count = 0;
   // create cloth
-  btScalar s = 5;
-  btScalar z = 30;
+  btScalar s = 1;
+  btScalar z = .1;
   btSoftBody* psb =
     btSoftBodyHelpers::CreatePatch(env->bullet->softBodyWorldInfo,
 				   btVector3(-s,-s,z),
@@ -35,12 +38,45 @@ ChooseActionScene::ChooseActionScene() {
   //pm->m_flags -= btSoftBody::fMaterial::DebugDraw;
   psb->generateBendingConstraints(2, pm);
   psb->setTotalMass(150);
-  env->add(BulletSoftObject::Ptr(new BulletSoftObject(psb)));  
+  cloth = BulletSoftObject::Ptr(new BulletSoftObject(psb));
+  env->add(cloth);
 }
 
 void ChooseActionScene::step(float dt, int maxsteps, float internaldt) {
-  // log stuff
-  // step parent
+  if (step_count % 300 == 0) {
+    btSoftBody * const psb = cloth->softBody.get();
+    btVector3 pos = psb->m_nodes[23].m_x;
+    btVector3 above = btVector3(pos.x(), pos.y(), pos.z() + 5);
+    btVector3 below = btVector3(pos.x(), pos.y(), pos.z() - 5);
+    btSoftBody::sRayCast s;
+    s.body = psb;
+    s.fraction = 1.f;
+    s.feature = btSoftBody::eFeature::None;
+    s.index = -1;
+    cout << psb->rayTest(above, below, s.fraction, s.feature, s.index, true);
+    cout << "\n";
+    cout << s.body;
+    cout << "\n";
+    cout << psb;
+    cout << "\n";
+    cout << s.index;
+    cout << "\n";
+    cout << s.feature;
+    cout << "\n";
+    cout << s.fraction;
+    cout << "\n";
+    /*for (int i = 0; i < psb->m_nodes.size(); i++) {
+      btVector3 pos = psb->m_nodes[i].m_x;
+      cout << pos.x();
+      cout << " ";
+      cout << pos.y();
+      cout << " ";
+      cout << pos.z();
+      cout << "\n";
+      }*/
+  }
+  step_count++;
+  BaseScene::step(dt, maxsteps, internaldt);
 }
 
 int main(int argc, char *argv[]) {
