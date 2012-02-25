4e93fc3026c60a1e22232b90feb3b7b25d459499
Ben Sklaroff <bsklaroff@gmail.com>
2012-02-24 17:46:14 -0800

    writes a softbody to a file

--- src/tests/test_choose_action.cpp
+++ src/tests/test_choose_action.cpp
@@ -1,6 +1,7 @@
 #include "simulation/basescene.h"
 #include "simulation/softbodies.h"
 #include "simulation/config_bullet.h"
+#include "simulation/bullet_io.h"
 #include <BulletSoftBody/btSoftBodyHelpers.h>
 #include <fstream>
 
@@ -9,19 +10,185 @@ struct ChooseActionScene : public BaseScene {
   BulletSoftObject::Ptr cloth;
 };
 
-void saveNodes(btVector3* nodes, const char* fileName) {
-  ofstream nodeFile;
-  nodeFile.open(fileName);
-  int len = sizeof(nodes) / sizeof(nodes[0]);
-  for (int i = 0; i < len; i++) {
-    nodeFile << nodes[i].x();
-    nodeFile << " ";
-    nodeFile << nodes[i].y();
-    nodeFile << " ";
-    nodeFile << nodes[i].z();
-    nodeFile << "\n";
+ostream &operator<<(ostream &stream, const btSoftBody::Material* mat) {
+  stream << mat->m_flags << " ";
+  stream << mat->m_kAST << " ";
+  stream << mat->m_kLST << " ";
+  stream << mat->m_kVST;
+  return stream;
+}
+
+ostream &operator<<(ostream &stream, const btSoftBody::Node* node) {
+  stream << node->m_x << " ";
+  stream << node->m_area << " ";
+  stream << node->m_battach << " ";
+  stream << node->m_f << " ";
+  stream << node->m_im << " ";
+  stream << node->m_n << " ";
+  stream << node->m_q << " ";
+  stream << node->m_v << " ";
+  stream << node->m_material;
+  return stream;
+}
+
+void saveSoftBody(const btSoftBody* orig, const char* fileName) {  
+  int i, j;
+  ofstream saveFile;
+  saveFile.open(fileName);
+
+  // materials
+  saveFile << orig->m_materials.size() << endl;
+  for (i = 0;i < orig->m_materials.size(); i++) {
+    const btSoftBody::Material *mat = orig->m_materials[i];
+    saveFile << mat << endl;
   }
-  nodeFile.close();
+
+  // nodes
+  saveFile << orig->m_nodes.size() << endl;
+  for (i = 0; i < orig->m_nodes.size(); i++) {
+    const btSoftBody::Node *node = &orig->m_nodes[i];
+    saveFile << node << endl;
+  }
+  
+  // links
+  saveFile << orig->m_links.size() << endl;
+  for (i = 0; i < orig->m_links.size(); i++) {
+    const btSoftBody::Link *link = &orig->m_links[i];
+    saveFile << link->m_material << endl;
+    saveFile << link->m_n[0] << endl;
+    saveFile << link->m_n[1] << endl;
+    saveFile << link->m_bbending << " ";
+    saveFile << link->m_rl << endl;
+  }
+
+  // faces
+  saveFile << orig->m_faces.size();
+  for (i = 0; i < orig->m_faces.size(); i++) {
+    const btSoftBody::Face *face = &orig->m_faces[i];
+    saveFile << face->m_material << endl;
+    saveFile << face->m_n[0] << endl;
+    saveFile << face->m_n[1] << endl;
+    saveFile << face->m_n[2] << endl;
+    saveFile << face->m_normal << " ";
+    saveFile << face->m_ra << endl;
+  }
+
+  // pose
+  saveFile << orig->m_pose.m_bvolume << " ";
+  saveFile << orig->m_pose.m_bframe << " ";
+  saveFile << orig->m_pose.m_volume << endl;
+  saveFile << orig->m_pose.m_pos.size() << endl;
+  for (i = 0; i < orig->m_pose.m_pos.size(); i++) {
+    saveFile << orig->m_pose.m_pos[i] << endl;
+  }
+  saveFile << orig->m_pose.m_wgh.size() << endl;
+  for (i = 0; i < orig->m_pose.m_wgh.size(); i++) {
+    saveFile << orig->m_pose.m_wgh[i] << endl;
+  }
+  saveFile << orig->m_pose.m_com << " ";
+  saveFile << orig->m_pose.m_rot << " ";
+  saveFile << orig->m_pose.m_scl << " ";
+  saveFile << orig->m_pose.m_aqq << endl;
+
+  // config
+  saveFile << orig->m_cfg.aeromodel << " ";
+  saveFile << orig->m_cfg.kVCF << " ";
+  saveFile << orig->m_cfg.kDP << " ";
+  saveFile << orig->m_cfg.kDG << " ";
+  saveFile << orig->m_cfg.kLF << " ";
+  saveFile << orig->m_cfg.kPR << " ";
+  saveFile << orig->m_cfg.kVC << " ";
+  saveFile << orig->m_cfg.kDF << " ";
+  saveFile << orig->m_cfg.kMT << " ";
+  saveFile << orig->m_cfg.kCHR << " ";
+  saveFile << orig->m_cfg.kKHR << " ";
+  saveFile << orig->m_cfg.kSHR << " ";
+  saveFile << orig->m_cfg.kAHR << " ";
+  saveFile << orig->m_cfg.kSRHR_CL << " ";
+  saveFile << orig->m_cfg.kSKHR_CL << " ";
+  saveFile << orig->m_cfg.kSSHR_CL << " ";
+  saveFile << orig->m_cfg.kSR_SPLT_CL << " ";
+  saveFile << orig->m_cfg.kSK_SPLT_CL << " ";
+  saveFile << orig->m_cfg.kSS_SPLT_CL << " ";
+  saveFile << orig->m_cfg.maxvolume << " ";
+  saveFile << orig->m_cfg.timescale << " ";
+  saveFile << orig->m_cfg.viterations << " ";
+  saveFile << orig->m_cfg.piterations << " ";
+  saveFile << orig->m_cfg.diterations << " ";
+  saveFile << orig->m_cfg.citerations << " ";
+  saveFile << orig->m_cfg.collisions << endl;
+  saveFile << orig->m_cfg.m_vsequence.size() << endl;
+  for (i = 0; i < orig->m_cfg.m_vsequence.size(); i++) {
+    saveFile << orig->m_cfg.m_vsequence[i] << endl;
+  }
+  saveFile << orig->m_cfg.m_psequence.size() << endl;
+  for (i = 0; i < orig->m_cfg.m_psequence.size(); i++) {
+    saveFile << orig->m_cfg.m_psequence[i] << endl;
+  }
+  saveFile << orig->m_cfg.m_dsequence.size() << endl;
+  for (i = 0; i < orig->m_cfg.m_dsequence.size(); i++) {
+    saveFile << orig->m_cfg.m_dsequence[i] << endl;
+  }
+  saveFile << orig->getCollisionShape()->getMargin() << endl;
+
+  // solver state
+  saveFile << orig->m_sst.isdt << " ";
+  saveFile << orig->m_sst.radmrg << " ";
+  saveFile << orig->m_sst.sdt << " ";
+  saveFile << orig->m_sst.updmrg << " ";
+  saveFile << orig->m_sst.velmrg << endl;
+  
+  // clusters
+  saveFile << orig->m_clusters.size() << endl;
+  for (i = 0; i < orig->m_clusters.size(); i++) {
+    btSoftBody::Cluster *cl = orig->m_clusters[i];
+    saveFile << cl->m_nodes.size() << endl;
+    for (j = 0; j < cl->m_nodes.size(); j++)
+      saveFile << cl->m_nodes[j] << endl;
+    saveFile << cl->m_masses.size() << endl;
+    for (j = 0; j < cl->m_masses.size(); j++)
+      saveFile << cl->m_masses[j] << endl;
+    saveFile << cl->m_framerefs.size() << endl;
+    for (j = 0; j < cl->m_framerefs.size(); j++)
+      saveFile << cl->m_framerefs[j] << endl;
+    saveFile << cl->m_framexform << " ";
+    saveFile << cl->m_idmass << " ";
+    saveFile << cl->m_imass << " ";
+    saveFile << cl->m_locii << " ";
+    saveFile << cl->m_invwi << " ";
+    saveFile << cl->m_com << " ";
+    saveFile << cl->m_vimpulses[0] << " ";
+    saveFile << cl->m_vimpulses[1] << " ";
+    saveFile << cl->m_dimpulses[0] << " ";
+    saveFile << cl->m_dimpulses[1] << " ";
+    saveFile << cl->m_nvimpulses << " ";
+    saveFile << cl->m_ndimpulses << " ";
+    saveFile << cl->m_lv << " ";
+    saveFile << cl->m_av << " ";
+    saveFile << cl->m_ndamping << " ";
+    saveFile << cl->m_ldamping << " ";
+    saveFile << cl->m_adamping << " ";
+    saveFile << cl->m_matching << " ";
+    saveFile << cl->m_maxSelfCollisionImpulse << " ";
+    saveFile << cl->m_selfCollisionImpulseFactor << " ";
+    saveFile << cl->m_containsAnchor << " ";
+    saveFile << cl->m_collide << " ";
+    saveFile << cl->m_clusterIndex << endl;
+  }
+
+  // cluster connectivity
+  saveFile << orig->m_clusterConnectivity.size() << endl;
+  for (i = 0; i < orig->m_clusterConnectivity.size(); i++) {
+    saveFile << orig->m_clusterConnectivity[i] << endl;
+  }
+
+  saveFile.close();
+}
+
+btSoftBody* loadSoftBody(const char* fileName) {
+  ifstream loadFile;
+  loadFile.open(fileName);
+  loadFile.close();
 }
 
 btSoftBody* createPatch(btSoftBodyWorldInfo& worldInfo,
@@ -31,7 +198,6 @@ btSoftBody* createPatch(btSoftBodyWorldInfo& worldInfo,
 			int fixeds,
 			bool gendiags) {
 #define IDX(_x_,_y_) ((_y_)*rx+(_x_))
-  saveNodes(x, "testfile");
   /* Create nodes */
   if(sizeof(x) < 1) return(0);
   const int rx = resx;
@@ -117,11 +283,11 @@ ChooseActionScene::ChooseActionScene() {
       }
     }
   }
-
+  /*
   btSoftBody* psb = createPatch(env->bullet->softBodyWorldInfo,
 				nodes, 31, 31, 0, true);
 
-  /*
+  */
   btSoftBody* psb = createPatch(env->bullet->softBodyWorldInfo,
 				btVector3(-s,-s,z),
 				btVector3(+s,-s,z),
@@ -129,13 +295,14 @@ ChooseActionScene::ChooseActionScene() {
 				btVector3(+s,+s,z),
 				31, 31,
 				0, true);
-  */
+
   psb->getCollisionShape()->setMargin(0.4);
   btSoftBody::Material* pm=psb->appendMaterial();
   pm->m_kLST = 0.4;
   //pm->m_flags -= btSoftBody::fMaterial::DebugDraw;
   psb->generateBendingConstraints(2, pm);
   psb->setTotalMass(150);
+  saveSoftBody(psb, "testfile.txt");
   cloth = BulletSoftObject::Ptr(new BulletSoftObject(psb));
   env->add(cloth);
 }
@@ -219,14 +386,14 @@ int main(int argc, char *argv[]) {
 	btVector3 pos = psb->m_nodes[i].m_x;
 	btVector3 above = btVector3(pos.x(), pos.y(), pos.z() + 5);
 	btVector3 below = btVector3(pos.x(), pos.y(), pos.z() - 5);
-	
+	/*
 	cout << i;
 	cout << " ";
 	cout << rayTest(psb, above, below);
 	cout << ", ";
-	
+	*/
       }
-      cout << "\n";
+      //cout << "\n";
     }
     step_count++;
     scene.step(.01);
