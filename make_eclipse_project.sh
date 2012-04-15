dc0616262bef3339222d663225587ee7bb26ccee
John Schulman <john.d.schulman@gmail.com>
2012-04-15 12:46:39 -0700

    make eclipse pro script

---
+++ make_eclipse_project.sh
@@ -0,0 +1,4 @@
+rm -rf ~/build/bulletsim_eclipse
+mkdir ~/build/bulletsim_eclipse
+cd ~/build/bulletsim_eclipse
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_PERCEPTION=ON -DJOHNS_ADVENTURES=ON ~/bulletsim
