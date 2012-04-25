132b2f01baac7669e0e105be380056d7b026cd7d
Jonathan Ho <jonathanho@berkeley.edu>
2012-04-24 19:44:52 -0700

    log4cplus

---
+++ src/tests/test_logging.cpp
@@ -0,0 +1,10 @@
+#include "simulation/logging.h"
+
+int main(int argc, char *argv[]) {
+    LoggingInit();
+    LOG_ERROR("oh no");
+    LOG_FATAL("fatal");
+    LOG_INFO("info");
+    LOG_TRACE("trace");
+    return 0;
+}
