64e036396536d71f20a374b5637ea441d6f1d05e
Sameep Tandon <tandon@eecs.berkeley.edu>
2012-09-16 11:03:15 -0700

    ilqr solver

---
+++ src/bscp/apps/car_bsp_ilqr.cpp
@@ -0,0 +1,225 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include "robots.h"
+#include "car.h"
+#include "beacon_sensor.h"
+#include "eigen_io_util.h"
+#include "osg_util.h"
+#include <osgViewer/Viewer>
+#include <osgViewer/View>
+#include <osgViewer/CompositeViewer>
+#include <osg/Group> 
+#include <osg/Camera>
+#include <osg/GraphicsContext>
+#include <osg/Geode>
+#include <osg/StateSet>
+#include <osg/Math>
+#include <osg/Geometry>
+#include <osg/MatrixTransform>
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "kalman_filter.h"
+#include "scp_solver.h"
+#include "ilqr_solver.h"
+#include "timer.h"
+#include "eigen_multivariate_normal.h"
+#include "trajectory_util.h"
+#include "sensor_functions/CarBeaconFunc.h"
+
+using namespace Eigen;
+using namespace std;
+
+
+static VectorXd _goal_offset;
+
+VectorXd GoalFn(Robot &r, const VectorXd& x) {
+	VectorXd ret(x.rows()-2);
+	ret.segment(0,2) = 10*(x.segment(0,2) - _goal_offset.segment(0,2));
+	ret.segment(2,ret.rows()-2) = x.segment(4,x.rows()-4);
+	return  ret;
+}
+
+int main()
+{
+  // initialize random number generator
+  srand(time(NULL));
+
+  // initialize plotting
+  osg::Group* root = new osg::Group();
+  osg::Group* traj_group = new osg::Group();
+  root->addChild(traj_group);
+  init_transparency_group(traj_group);
+  vector<osg::Node*> render;
+  double z_offset = 0.01;
+
+  Vector4d yellow(1.0,1.0,0.1,0.4);  
+  Vector4d red(1.0, 0.0, 0.0, 0.8); 
+  Vector4d blue(0.2, 0.2, 1.0, 0.8);
+  Vector4d orange(1.0, 0.55, 0.0, 0.8);
+  Vector4d white(1.0, 1.0, 1.0, 0.1); 
+  Vector4d brown(139/255.0,69/255.0,19/255.0,0.8);
+  Vector4d transparent(0.0,0.0,0.0,0.0);
+
+
+  // intialize the robot
+  int T = 20;
+  int NX = 4;
+  int NU = 2;
+  int NB = NX*(NX+3)/2;
+  int NS = 0;
+  int NUM_TEST = 0;
+  double rho_x = 0.1; 
+  double rho_u = 0.1;
+  int N_iter = 20;
+
+  // hack for now
+  MatrixXd W_cov = MatrixXd::Identity(NB,NB);
+  EigenMultivariateNormal<double> sampler(VectorXd::Zero(NB), W_cov);
+  
+  VectorXd x0(NX);
+  x0 << 0.0, 0.0, 0.0, -0.3;
+  Car c(x0);
+  c.greet();
+
+  //initialize the sensors
+  int NZ = 3; 
+  Vector2d beacon_1_pos(-0.2,0.3); 
+  BeaconSensor s1(beacon_1_pos);
+  s1.draw(beacon_1_pos, brown, traj_group);
+  Robot::SensorFunc s1_f = &CarBeaconFunc;
+  c.attach_sensor(&s1, s1_f);
+
+  Vector2d beacon_2_pos(0.2,0.1); 
+  BeaconSensor s2(beacon_2_pos);
+  s2.draw(beacon_2_pos, brown, traj_group);
+  Robot::SensorFunc s2_f = &CarBeaconFunc;
+  c.attach_sensor(&s2, s2_f);
+
+  Vector2d beacon_3_pos(-0.3,0.1); 
+  BeaconSensor s3(beacon_3_pos);
+  s3.draw(beacon_3_pos, brown, traj_group);
+  Robot::SensorFunc s3_f = &CarBeaconFunc;
+  c.attach_sensor(&s3, s3_f);
+  
+
+  //initilaize the initial distributions and noise models
+  MatrixXd Sigma_0 = 0.0001*MatrixXd::Identity(NX,NX);
+  Sigma_0(2,2) = 0.0000000001;
+  Sigma_0(3,3) = 0.000000001;
+  LLT<MatrixXd> lltSigma_0(Sigma_0);
+  MatrixXd rt_Sigma_0 = lltSigma_0.matrixL();
+
+  MatrixXd Q_t = 0.000001*MatrixXd::Identity(NX,NX);
+  Q_t(2,2) = 0.0000001;
+  Q_t(3,3) = 0.0000000001;
+  LLT<MatrixXd> lltQ_t(Q_t);
+  MatrixXd M_t = lltQ_t.matrixL(); 
+
+  MatrixXd R_t = 0.05*MatrixXd::Identity(NZ,NZ);
+  LLT<MatrixXd> lltR_t(R_t);
+  MatrixXd N_t = lltR_t.matrixL();
+  s1.setN(N_t(0,0));
+  s2.setN(N_t(1,1));
+  s3.setN(N_t(2,2));
+
+  //Set car noise models
+  c.set_M(M_t);
+
+  cout << "models set" << endl;
+
+  VectorXd b_0;
+  build_belief_state(x0, rt_Sigma_0, b_0);
+  vector<VectorXd> B_bar(T+1);
+  vector<VectorXd> U_bar(T);
+  vector<MatrixXd> W_bar(T); 
+  B_bar[0] = b_0; 
+
+
+  for (int t = 0; t < T; t++) {
+    VectorXd u = VectorXd::Random(2) / 3;
+    if (t > T/2) u = - 2 * u;  
+    U_bar[t] = u;
+    MatrixXd rt_W_t;
+    c.belief_dynamics(B_bar[t], U_bar[t], B_bar[t+1]);
+    c.belief_noise(B_bar[t], U_bar[t], rt_W_t);
+    //sampler.setCovar(rt_W_t * rt_W_t.transpose()); THIS MUST BE NORMAL SAMPLES NOW
+    sampler.generateSamples(W_bar[t], NS);
+    //cout << W_bar[t] << endl;
+  }
+
+  cout << "initial trajectory set" << endl;
+
+  vector<vector<VectorXd> > W_s_t;
+  index_by_sample(W_bar, W_s_t);
+//  vector<VectorXd> test;
+//  for (int s = 0; s < NS; s++) { THIS BE BROKEN BECAUSE W_S_T IS THE IDENTITY DISTRIBUTION
+//	  c.forward_integrate(b_0, U_bar, W_s_t[s], test);
+//	  render = c.draw_belief_trajectory(test, red, transparent, traj_group, 1e-4);
+//  }
+
+
+  render = c.draw_belief_trajectory(B_bar, red, yellow, traj_group, 1e-4);
+
+  // setup for SCP
+  // Define a goal state
+  VectorXd b_goal = B_bar[T];
+  b_goal.segment(NX, NB-NX) = VectorXd::Zero(NB-NX); // trace
+  _goal_offset = b_goal;
+  // Output variables
+  vector<VectorXd> opt_B, opt_U; // noiseless trajectory
+  MatrixXd Q; VectorXd r;  // control policy
+
+  cout << "calling ilqr" << endl;
+  ilqr_solver(c, B_bar, U_bar, W_bar, rho_x, rho_u, b_goal.segment(0,NX), N_iter,
+      opt_B, opt_U, Q, r);
+
+  c.draw_belief_trajectory(opt_B, blue, orange, traj_group, z_offset/2 +1e-4);
+
+//
+//  TrajectoryInfo opt_traj(b_0, &GoalFn, NULL);
+//    for (int t = 0; t < T; t++) {
+//  	  //opt_traj.add_and_integrate(opt_U[t], VectorXd::Zero(NB), c);
+//  	  VectorXd feedback = opt_traj.Q_feedback(c);
+//  	  VectorXd u_policy = Q.block(t*NU, t*NB, NU, NB) * feedback + r.segment(t*NU, NU);
+//  	  opt_traj.add_and_integrate(u_policy, VectorXd::Zero(NB), c);
+//    }
+//    c.draw_belief_trajectory(opt_traj._X, blue, orange, traj_group, z_offset/2+1e-4);
+//    for (int i = 0; i < T; i++)
+//    	cout << opt_traj._X[i].transpose() << endl;
+//
+//    for (int s = 0; s < NUM_TEST; s++) {
+//  	  TrajectoryInfo test_traj(b_0, &GoalFn, NULL);
+//  	  for (int t = 0; t < T; t++) {
+//  		  VectorXd feedback = test_traj.Q_feedback(c);
+//  	  	  VectorXd u_policy = Q.block(t*NU, t*NB, NU, NB) * feedback + r.segment(t*NU, NU);
+//		  MatrixXd rt_W_t;
+//		  c.belief_noise(opt_traj._X[t], opt_traj._U[t], rt_W_t);
+//		  sampler.setCovar(rt_W_t * rt_W_t.transpose());
+//  		  //test_traj.add_and_integrate(u_policy, W_bar[t].col(s), c);
+//  		  test_traj.add_and_integrate(u_policy, sampler.nextSample(), c);
+//  	  }
+//  	  c.draw_belief_trajectory(test_traj._X, blue, transparent, traj_group, z_offset/2+1e-4);
+//    }
+
+
+
+  // visualize
+  osg::Geode * fgeode = new osg::Geode; 
+  osg::ShapeDrawable *floor = new osg::ShapeDrawable(new osg::Box(osg::Vec3(0.0,0.0,-z_offset/2), 1.0, 1.0, z_offset));
+  floor->setColor(osg::Vec4(0.1,0.1,0.1,1.0));
+  fgeode->addDrawable(floor);
+  root->addChild(fgeode);
+
+  // create a context that spans the entire x screen
+  int width = 800;
+  int height = 800;
+  osg::ref_ptr<osgViewer::CompositeViewer> compositeViewer = new osgViewer::CompositeViewer;
+
+  osgViewer::View* v0 = new osgViewer::View();
+  v0->setSceneData(root);
+  v0->setUpViewInWindow(0,0,width,height);
+  compositeViewer->addView(v0);
+
+  return compositeViewer->run();
+}
---
+++ src/bscp/lib/opt/ilqr_solver.h
@@ -0,0 +1,33 @@
+#ifndef _ilqr_solver_h
+#define _ilqr_solver_h
+
+#include "robots.h"
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+
+/*
+ * ilqr_solver interface is as follows:
+ * Robot r: used for abstracting the dynamics and observation models. %TODO: pass in a simulation environment instead. 
+ * X_bar: a nominal trajectory of states with length T+1
+ * U_bar: a nominal trajectory of controls with length T
+ * W_bar: a nominal trajectory of noise samples, each matrix is NX*NS; vector of length T
+ * rho_x: trust region constraint for noiseless state trajectory
+ * rho_u: trust region constraint for noiseless control trajectory
+ * x_goal: desired state
+ * N_iter: number of iterations to run SCP
+ * opt_X: return value containing states for noiseless trajectory
+ * opt_U: return value containing controls for noiseless trajectory
+ * K: return value containing control policy (Linear feedback part)
+ * u0: return value containing control policy (affine offset part)
+ */
+
+void ilqr_solver(Robot &r, const vector<VectorXd>& B_bar,
+		const vector<VectorXd>& U_bar, const vector<MatrixXd>& W_bar,
+		const double rho_x, const double rho_u, const VectorXd goal_mu,
+		const int N_iter, vector<VectorXd>& opt_X, vector<VectorXd>& opt_U,
+		MatrixXd& K, VectorXd& u0);
+
+double computeExpectedCost(Robot &r, const vector<VectorXd>& B_ilqr,
+		const vector<VectorXd>& U_ilqr, const vector<MatrixXd>& L, const VectorXd& goal_mu);
+
+#endif
