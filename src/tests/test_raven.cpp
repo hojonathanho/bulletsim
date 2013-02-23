875f0dbf2dc3cf12043fc6ca86637a8b13f4de97
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-02-23 14:24:51 -0800

    Changed path to raven file.

--- src/tests/test_raven.cpp
+++ src/tests/test_raven.cpp
@@ -1,12 +1,12 @@
 #include "simulation/simplescene.h"
-#include "robots/pr2.h"
+
 typedef boost::function<void(void)> VoidCallback;
 int main(int argc, char* argv[]) {
   Parser parser;
   parser.addGroup(GeneralConfig());
   parser.read(argc, argv);
   Scene scene;
-  Load(scene.env, scene.rave, "/home/joschu/bulletsim/data/xml/raven_env.xml",true);
+  Load(scene.env, scene.rave, "/home/sibi/sandbox/bulletsim/data/xml/raven_env.xml",false);
 //  PR2Manager pr2m(scene);
   scene.startViewer();
   scene.startLoop();
