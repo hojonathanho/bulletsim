1a26de8f31700eea6791681ea32cef4957b800ea
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-02-23 14:30:04 -0800

    Added test_suture_cloth.cpp

---
+++ src/test_suture_cloth.cpp
@@ -0,0 +1,31 @@
+#include "simulation/simplescene.h"
+#include "simulation/softbodies.h"
+#include "simulation/config_bullet.h"
+#include "simulation/softBodyHelpers.h"
+#include "simulation/basicobjects.h"
+#include <BulletSoftBody/btSoftBodyHelpers.h>
+#include <openrave/kinbody.h>
+#include "robots/pr2.h"
+#include "CustomScene.h"
+
+
+
+
+int main(int argc, char *argv[]) {
+	GeneralConfig::scale = 20.;
+	ViewerConfig::cameraHomePosition = btVector3(100, 0, 100);
+	BulletConfig::dt = 0.01;
+	BulletConfig::internalTimeStep = 0.01;
+	BulletConfig::maxSubSteps = 0;
+
+	Parser parser;
+
+	parser.addGroup(GeneralConfig());
+	parser.addGroup(BulletConfig());
+	parser.addGroup(SceneConfig());
+	parser.read(argc, argv);
+
+	CustomScene().run();
+	return 0;
+}
+
