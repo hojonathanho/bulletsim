cbc180649fd0acff09d2e3a9eb485152cfa86b64
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-03-10 19:24:23 -0700

    Added command line arguments for box cloth.

--- src/tests/ravens/ravens_config.h
+++ src/tests/ravens/ravens_config.h
@@ -6,9 +6,19 @@ struct RavenConfig : Config {
   static int cloth;
   static float record_freq;
 
+  static int bcN;
+  static int bcM;
+  static float bcS;
+  static float bcH;
+
   RavenConfig() : Config() {
     params.push_back(new Parameter<int>("cloth", &cloth, "include cloth in screen (1/0)"));
     params.push_back(new Parameter<float>("record_freq", &record_freq, "frequency of recording"));
+	params.push_back(new Parameter<int>("bcN", &bcN, "BoxCloth: number of squares in x direction"));
+    params.push_back(new Parameter<int>("bcM", &bcM, "BoxCloth: number of squares in y direction"));
+    params.push_back(new Parameter<float>("bcS", &bcS, "BoxCloth: square side length"));
+    params.push_back(new Parameter<float>("bcH", &bcH, "BoxCloth: box object height"));
+
   }
 
 };
