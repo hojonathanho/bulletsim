ba36e9603fe3ad154d9cdf454a8c293b5c9ddbb8
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-03-11 23:20:05 -0700

    Added new command line options.

--- src/tests/ravens/ravens_config.cpp
+++ src/tests/ravens/ravens_config.cpp
@@ -2,6 +2,7 @@
 
 int RavenConfig::cloth = 0;
 float RavenConfig::record_freq = 5.0;
+int RavenConfig::lfdProcessing = 0;
 int RavenConfig::bcN = 5;
 int RavenConfig::bcM = 5;
 float RavenConfig::bcS = 0.005;
--- src/tests/ravens/ravens_config.h
+++ src/tests/ravens/ravens_config.h
@@ -5,6 +5,7 @@
 struct RavenConfig : Config {
   static int cloth;
   static float record_freq;
+  static int lfdProcessing;
 
   static int bcN;
   static int bcM;
@@ -14,6 +15,7 @@ struct RavenConfig : Config {
   RavenConfig() : Config() {
     params.push_back(new Parameter<int>("cloth", &cloth, "include cloth in screen (1/0)"));
     params.push_back(new Parameter<float>("record_freq", &record_freq, "frequency of recording"));
+    params.push_back(new Parameter<int>("lfdProcessing", &lfdProcessing, "perform lfd processing"));
 	params.push_back(new Parameter<int>("bcN", &bcN, "BoxCloth: number of squares in x direction"));
     params.push_back(new Parameter<int>("bcM", &bcM, "BoxCloth: number of squares in y direction"));
     params.push_back(new Parameter<float>("bcS", &bcS, "BoxCloth: square side length"));
