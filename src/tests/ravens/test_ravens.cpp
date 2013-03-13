c09b51e3582e38a78ec64207c39f557289e609a1
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-03-12 23:30:08 -0700

    Added a bunch of default variables to de-clog command line.

--- src/tests/ravens/test_ravens.cpp
+++ src/tests/ravens/test_ravens.cpp
@@ -11,11 +11,18 @@
 
 
 int main(int argc, char *argv[]) {
-	GeneralConfig::scale = 20.;
-	ViewerConfig::cameraHomePosition = btVector3(100, 0, 100);
+	ViewerConfig::cameraHomePosition = btVector3(0, 0.6, 0.5);
+	ViewerConfig::cameraHomeCenter = btVector3(0, 0, 0.25);
 	BulletConfig::dt = 0.01;
-	BulletConfig::internalTimeStep = 0.01;
-	BulletConfig::maxSubSteps = 0;
+
+	BulletConfig::internalTimeStep = 0.001;
+	BulletConfig::maxSubSteps = 10;
+	BulletConfig::gravity = -1*btVector3(0,0,10);
+	RavenConfig::record_freq = 100;
+	RavenConfig::cloth = 0;
+	SceneConfig::enableHaptics = 1;
+	SceneConfig::enableShadows = 1;
+	GeneralConfig::scale = 100.;
 
 	Parser parser;
 
