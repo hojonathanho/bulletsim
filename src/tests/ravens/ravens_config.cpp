617cc176d63e1b07567bfea4af62b671a75012af
Sibi Venkatesan <sibi.venkatesan@gmail.com>
2013-03-21 22:12:52 -0700

    Changed parameters for box cloth.

--- src/tests/ravens/ravens_config.cpp
+++ src/tests/ravens/ravens_config.cpp
@@ -2,8 +2,8 @@
 
 int   RavenConfig::cloth = 0;
 float RavenConfig::record_freq = 5.0;
-int   RavenConfig::bcN = 8;
-int   RavenConfig::bcM = 5;
+int   RavenConfig::bcN = 5;
+int   RavenConfig::bcM = 8;
 float RavenConfig::bcS = 0.01;
 float RavenConfig::bcH = 0.0015;
 bool RavenConfig::enableLfd = false;
