867119a096256845dc81ece1673eebea59849685
Ankush Gupta <ankush.me@hotmail.com>
2013-07-27 19:39:32 -0700

    interpreting results script updated

--- src/tests/ravens/interpret_res.py
+++ src/tests/ravens/interpret_res.py
@@ -4,6 +4,7 @@ import numpy as np
 perturb_scale = 1
 runrange = (1200, 1263)
 
+np.set_printoptions(precision=3)
 
 dir  = "/media/0846DE7846DE65CA/suturing_runs/runs_%d_2" % perturb_scale
 resf = dir  + "/results_%d_2.txt" % perturb_scale
@@ -25,10 +26,9 @@ for line in resf:
     else:
         res_dict[runnum]['comm'] = ""
 
-for i in xrange(runrange[0], runrange[1]):
+for i in xrange(runrange[0], runrange[1]+1):
     ff = open(fformat%i, 'rb')
     irun_dict = cPickle.load(ff)
-    
 
     max_warp_cost= max([seg['warp_costs'][0] + seg['warp_costs'][1] for seg in irun_dict['segments_info']])  
     res_dict[i]['hamm'] = np.count_nonzero(irun_dict['perturbations'])
@@ -36,11 +36,6 @@ for i in xrange(runrange[0], runrange[1]):
     res_dict[i]['max_warp'] = max_warp_cost
     
    
-#import pprint
-#pp = pprint.PrettyPrinter(depth=6)
-#pp.pprint(res_dict)
-
-import yaml
-interpf = open(interp_fname, 'w')
-interpf.write(yaml.dump(res_dict, default_flow_style=False))
-interpf.close()
+import pprint
+pp = pprint.PrettyPrinter(depth=6)
+pprint.pprint(res_dict, open(interp_fname, 'w'))
